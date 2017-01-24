d5721dfe4dc2b2006e7016dc1ec262391c5915ae
Fabrizio Durante <fabrizio.durante294@gmail.com>
2017-01-24 23:31:50 +0100

    Prova Grafico (non funziona)

--- app/src/main/java/com/durante/fabrizio/myaccountant/PlaceholderFragment.java
+++ app/src/main/java/com/durante/fabrizio/myaccountant/PlaceholderFragment.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 /**
@@ -44,13 +45,40 @@ public class PlaceholderFragment extends Fragment {
         Saldo=(TextView)rootView.findViewById(R.id.tv_Saldo);
         graph = (GraphView)rootView.findViewById(R.id.graph);
         DBHelper db=new DBHelper(getContext());
-        LineGraphSeries<DataPoint> series = new LineGraphSeries();
+        LineGraphSeries<DataPoint> series = new LineGraphSeries(new DataPoint[]{
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
+        // activate horizontal zooming and scrolling
+        graph.getViewport().setScalable(true);
+
+// activate horizontal scrolling
+        graph.getViewport().setScrollable(true);
+
+// activate horizontal and vertical zooming and scrolling
+        graph.getViewport().setScalableY(true);
+
+// activate vertical scrolling
+        graph.getViewport().setScrollableY(true);
+
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(0.5);
+        graph.getViewport().setMaxX(3.5);
+
+// set manual Y bounds
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setMinY(3.5);
+        graph.getViewport().setMaxY(8);
 
         Cursor ris=db.get_Conto(getArguments().getInt("Conto"));
         if(!ris.moveToFirst()){
             Toast.makeText(getContext(), "Conto inesistente!", Toast.LENGTH_LONG).show();
             return null;
-            //DFFHGF
         }
         NomeConto.setText(ris.getString(0));
         Saldo.setText(ris.getString(1));
