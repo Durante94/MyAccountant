31773d7264c5a5958c8ca5b8254844d638d8de53
Fabrizio Durante <fabrizio.durante294@gmail.com>
2017-01-31 00:14:37 +0100

    Primo tentativo mappa

--- app/src/main/java/com/durante/fabrizio/myaccountant/MappaMovimento.java
+++ app/src/main/java/com/durante/fabrizio/myaccountant/MappaMovimento.java
@@ -1,5 +1,6 @@
 package com.durante.fabrizio.myaccountant;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
@@ -9,31 +10,40 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
-public class MappaMovimento extends AppCompatActivity {
+public class MappaMovimento extends AppCompatActivity implements OnMapReadyCallback {
 
     private TextView Data, Conto, Importo, Tipo;
-    private String mappa;
+    private GoogleMap mappa;
     private DBHelper db;
     private Bundle args;
-    private boolean mShowMAp;
-    private GoogleMap mMap;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mappa_movimento);
+        if(googleServicesAviable()){
+            setContentView(R.layout.activity_mappa_movimento);
+        }else {
+            Toast.makeText(this, "Impossibile creare la mappa", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(MappaMovimento.this, MainActivity.class));
+            return;
+        }
 
-        Data=(TextView)findViewById(R.id.tv_data_movimento);
-        Conto=(TextView)findViewById(R.id.tv_conto_movimento);
-        Importo=(TextView)findViewById(R.id.tv_importo_movimento);
-        Tipo=(TextView)findViewById(R.id.tv_tipo_movimento);
-        db=new DBHelper(this);
-        args=getIntent().getExtras();
-        //mShowMAp= GooglePlayServiceUtility.isPlayServiceAviable(this);
+        Data = (TextView) findViewById(R.id.tv_data_movimento);
+        Conto = (TextView) findViewById(R.id.tv_conto_movimento);
+        Importo = (TextView) findViewById(R.id.tv_importo_movimento);
+        Tipo = (TextView) findViewById(R.id.tv_tipo_movimento);
+        //mappa=(()findViewById(R.id.Mappa)).getMap();
+        db = new DBHelper(this);
+        args = getIntent().getExtras();
+
 
         Cursor ris=db.get_singol_Movim(args.getInt("Movim"));
         if(ris.getCount()<=0){
@@ -46,7 +56,8 @@ public class MappaMovimento extends AppCompatActivity {
         Conto.setText(ris.getString(4));
         Importo.setText(ris.getString(2));
         Tipo.setText(ris.getString(1));
-        mappa=ris.getString(3);
+
+        //String stringMap=ris.getString(3);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -78,5 +89,29 @@ public class MappaMovimento extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean googleServicesAviable(){
+        GoogleApiAvailability api=GoogleApiAvailability.getInstance();
+        int isAviable=api.isGooglePlayServicesAvailable(this);
+        if(isAviable== ConnectionResult.SUCCESS){
+            return true;
+        }else if (api.isUserResolvableError(isAviable)){
+            Dialog d=api.getErrorDialog(this, isAviable, 0);
+            d.show();
+        }else {
+            Toast.makeText(this, "Impossibile connettersi a Google Play Servicies", Toast.LENGTH_SHORT).show();
+        }
+        return false;
+    }
+
+    public void initMap(){
+        MapFragment map= (MapFragment)getFragmentManager().findFragmentById(R.id.Mappa);
+        map.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mappa=googleMap;
     }
 }
